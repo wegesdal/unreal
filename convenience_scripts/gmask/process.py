7e769dae0bace8c28ad6480713589dbf965f7e7c
William Egesdal <william.egesdal@gmail.com>
2019-01-14 20:47:12 -1000

    fixed script

--- convenience_scripts/gmask/process.py
+++ convenience_scripts/gmask/process.py
@@ -33,12 +33,7 @@ def main():
                 rkey -= reds
                 gkey -= greens
                 bkey -= blues
-                rkey = numpy.square(rkey)
-                gkey = numpy.square(gkey)
-                bkey = numpy.square(bkey)
-                square_distance = rkey + bkey + gkey
-                
-                mask = numpy.sqrt(square_distance) < int(sensitivity) #| (numpy.amax(rgb, axis=2) != greens)
+                mask = (abs(rkey) + abs(gkey) + abs(bkey)) < int(sensitivity) #| (numpy.amax(rgb, axis=2) != greens)
                 result = numpy.where(mask, 255, 0)
                 if not os.path.exists("masks"):
                     os.mkdir("masks")
