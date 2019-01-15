90df4a80bd25ae9a2818b43a5163e5b5a3c604a8
William Egesdal <william.egesdal@gmail.com>
2019-01-14 19:58:22 -1000

    chroma key script complete

--- convenience_scripts/gmask/process.py
+++ convenience_scripts/gmask/process.py
@@ -1,35 +1,43 @@
 
-            
-
-
-import cv2, numpy, sys, os, rawpy
+import cv2, numpy, sys, os, rawpy, math
 
 directory = os.getcwd()
 
 def main():
 
     if len(sys.argv) < 1:
-        print('input a sensitivity value as cl argument')
+        print('correct format is: python process.py sensitivity red green blue')
         main()
     else:
         for filename in os.listdir(directory):
+            key = (sys.argv[2], sys.argv[3], sys.argv[4])
             #cr2 is for Canon cameras, other cameras may need to be added to this conditional
-            if filename.endswith(".cr2"):
+            if filename.endswith(".CR2"): 
                 raw_image = filename
                 raw = rawpy.imread(raw_image)
                 rgb = raw.postprocess()
-                os.mkdir("tiffs")
-                cv2.imwrite(os.path.join(directory + "/tiffs", filename + ".tiff"), rgb)
+                if not os.path.exists("tiffs"):
+                    os.mkdir("tiffs")
+                #opencv uses bga instead of rgb so we need to invert the colors before saving the file
+                destRGB = cv2.cvtColor(rgb, cv2.COLOR_BGR2RGB)
+                cv2.imwrite(os.path.join(directory + "/tiffs", filename + ".tiff"), destRGB)
                 sensitivity = sys.argv[1]
                 empty_img = numpy.zeros_like(rgb)
                 RED, GREEN, BLUE = (2, 1, 0)
-                reds = img[:, :, RED]
-                greens = img[:, :, GREEN]
-                blues = img[:, :, BLUE]
-                mask = (greens < int(sensitivity)) | (numpy.amax(rgb, axis=2) != greens)
+                reds = rgb[:, :, RED]
+                greens = rgb[:, :, GREEN]
+                blues = rgb[:, :, BLUE]
+                rkey = numpy.ones_like(reds)*int(key[0])
+                gkey = numpy.ones_like(greens)*int(key[1])
+                bkey = numpy.ones_like(blues)*int(key[2])
+                rkey -= reds
+                gkey -= greens
+                bkey -= blues
+                mask = (rkey + bkey + gkey < int(sensitivity)) #| (numpy.amax(rgb, axis=2) != greens)
                 result = numpy.where(mask, 255, 0)
-                os.mkdir("masks")
-                cv2.imwrite(os.path.join(directory + "/masks", filename + "_mask.jpg"), result)
+                if not os.path.exists("masks"):
+                    os.mkdir("masks")
+                cv2.imwrite(os.path.join(directory + "/masks", filename + "_mask.tiff"), result)
                 continue
             else:
                 continue
